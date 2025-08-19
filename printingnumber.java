9812d6606bdef8205651ccc7cbceacfab593240b
payalk35228-hub <payalk35228@gmail.com>
2025-08-19 18:19:52 +0530

    Add files via upload

---
+++ printingnumber.java
@@ -0,0 +1,10 @@
+class printingnumber {
+    public static void main(String[]args) {
+        System.out.println("4");
+        System.out.println(4);
+        System.out.println(4+3);
+        System.out.println("4+3");
+    }
+}
+
+//("") iske andr jo bhi print hoga wo as a text print hoga
