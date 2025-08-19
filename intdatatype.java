d8776c92a389bdc73b37054c2dec2964f3f94a47
payalk35228-hub <payalk35228@gmail.com>
2025-08-19 18:18:48 +0530

    Add files via upload

---
+++ intdatatype.java
@@ -0,0 +1,10 @@
+public class intdatatype{
+    public static void main(String[] argss){
+        int x = 15;
+        int y = 6;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
