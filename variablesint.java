05b81c6867dffb98eae7f0ae3f632298b059305d
payalk35228-hub <payalk35228@gmail.com>
2025-08-19 18:20:19 +0530

    Add files via upload

---
+++ variablesint.java
@@ -0,0 +1,15 @@
+//printing variablles in java and updation of variables.
+
+public class variablesint{
+    public static void main(String[]argss){
+        int x;
+        x=5;
+        System.out.println(x);
+        x=7;
+        System.out.println(x);
+        x=x+6;
+        System.out.println(x);
+        x=x-20;
+        System.out.println(x);
+    }
+}
