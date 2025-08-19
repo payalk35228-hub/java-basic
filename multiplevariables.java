6df4dec0824f7f866d9ab1000e9cc66819b0db25
payalk35228-hub <payalk35228@gmail.com>
2025-08-19 18:19:22 +0530

    Add files via upload

---
+++ multiplevariables.java
@@ -0,0 +1,13 @@
+public class multiplevariables{
+    public static void main(String[]argss){
+        int x=5;
+        int y=6;
+        int z=7;
+        System.out.println("the value of x is 5");
+        System.out.println("the value of y is 6");
+        System.out.println("the value of z is 7");
+        
+        
+
+    }
+}
