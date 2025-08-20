665c40b644f4c42af092491c33fce5c471f251d4
payalk35228-hub <payalk35228@gmail.com>
2025-08-21 01:03:19 +0530

    Add files via upload

---
+++ areaofcircle.java
@@ -0,0 +1,10 @@
+public class areaofcircle{
+    public static void main(String[]argss){
+        double r =5;
+        double pi = 3.1415;
+        double a = (pi*r*r);
+        System.out.println("the area of circle is: "+a); 
+        // text+variable  me vriables text ke aage 
+        //jud jata hai, as text
+    }
+}
