2ce7116bd1e2436264dba7c3d8223420b6549abf
payalk35228-hub <payalk35228@gmail.com>
2025-08-21 01:03:45 +0530

    Add files via upload

---
+++ calculate4subject.java
@@ -0,0 +1,15 @@
+public class calculate4subject{
+    public static void main(String[]argss){
+        double x1;
+        x1=57;
+        double x2;
+        x2=45;
+        double x3;
+        x3=56;
+        double x4;
+        x4=35;
+        double p = (x1+x2+x3+x4/4);
+        System.out.println(p);
+    }
+
+}
