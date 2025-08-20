c0425284855e0138c97ce60fb11b77a9557a12f2
payalk35228-hub <payalk35228@gmail.com>
2025-08-21 01:04:07 +0530

    Add files via upload

---
+++ calculate5subject.java
@@ -0,0 +1,19 @@
+
+// calculating percentage        
+public class calculate5subject{
+    public static void main(String[]argss){
+        double x1; //x1 english
+        x1= 100.;
+        double x2; //x2 physics
+        x2= 67;
+        double x3; //x3 maths
+        x3= 76;
+        double x4; //x4 chemistry
+        x4= 85;
+        double x5; // computer
+        x5= 90;
+        double p = (x1+x2+x3+x4+x5/5);
+        System.out.println(p);
+
+    }
+}
