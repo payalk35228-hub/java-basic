905980bb13e0ebe70d1f65860fd6440216785c76
payalk35228-hub <payalk35228@gmail.com>
2025-08-21 01:05:32 +0530

    Add files via upload

---
+++ volumeofspehere.java
@@ -0,0 +1,11 @@
+//volume of spehere
+public class volumeofspehere {
+    public static void main(String[]argss){
+        double  radius = 12;
+        double volume = (4*3.14*radius *radius*radius/3);
+        System.out.println("the volume of spehre is :" +v);
+
+
+
+    }
+}
