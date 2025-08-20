3256e145b109339fc1af93b22fa3cad8b766c128
payalk35228-hub <payalk35228@gmail.com>
2025-08-21 01:04:41 +0530

    Add files via upload

---
+++ doubledatatype.java
@@ -0,0 +1,18 @@
+//public class doubledatatype{
+  //  public static void main(String[]argss){
+    //    double x=5;
+      //  double y=2;
+        //double z=x/y;
+        //System.out.println(z);
+
+  //  }
+//}
+public class doubledatatype{
+    public static void main(String[]argss){
+        double x = 5;
+        double y = 2;
+        double z = 5/2;
+        System.out.println(z);
+
+    }
+}
