ee2b5db61bfa67f4700d87f1b8caecd56a521bbd
payalk35228-hub <payalk35228@gmail.com>
2025-08-21 01:05:07 +0530

    Add files via upload

---
+++ simpleintrest.java
@@ -0,0 +1,10 @@
+public class  simpleintrest{
+    public static void main(String[]argss){
+        double principal = 100;
+        double rate = 10;
+        double time = 2;
+        double simple intrest = (principal*rate*time)/100;
+        System.out.println("the simple intrest is :"+simple intrest);
+    }
+
+}
