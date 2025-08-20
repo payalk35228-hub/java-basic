82e3225ac6744fa4efe9fea6e0a0ed0dd18bb394
payalk35228-hub <payalk35228@gmail.com>
2025-08-21 01:05:52 +0530

    Add files via upload

---
+++ sumof2numbers.java
@@ -0,0 +1,10 @@
+public class sumof2numbers{
+    public static void main(String[]argss){
+        int x;
+        x=12;
+        int y;
+        y=11;
+        int sum = x+y;
+        System.out.println(sum);
+    }
+}
