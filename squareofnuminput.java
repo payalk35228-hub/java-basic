c7d32be71bfdfa10d4f711e869fedcf6a70a784e
payalk35228-hub <payalk35228@gmail.com>
2025-08-23 19:46:49 +0530

    Add files via upload

---
+++ squareofnuminput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class squareofnuminput{
+    public static void main(String[]argss){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter a number :");
+    int a;
+    a = sc.nextInt();
+    int square = a*a;
+    System.out.println("sqaure of number that you gave is :"+square);
+    }
+
+
+
+
+}
