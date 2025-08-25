a286788c503fb27f5f045a46a0a702f78811d243
payalk35228-hub <payalk35228@gmail.com>
2025-08-25 13:14:41 +0530

    Add files via upload

---
+++ hierarchyinput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class hierarchyinput{
+    public static void main(String[]argss){
+        int i = 2, j=3, k, l;
+        double a, b;
+        k = i/j*j;
+        l = j/i*i;
+        a = i/j*j;
+        b = j/i*i;
+        System.out.println(k+" "+l+" "+a+" "+b);
+        
+
+    }
+}
